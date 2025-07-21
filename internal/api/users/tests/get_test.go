82a4c50071202ed279884d7bf2d21015c688422a
levon-dalakyan <wman8800@gmail.com>
2025-07-21 18:30:34 +0300

    add tests for get

--- internal/api/users/tests/get_test.go
+++ internal/api/users/tests/get_test.go
@@ -1,1 +1,118 @@
 package tests
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/levon-dalakyan/auth/internal/api/users"
+	"github.com/levon-dalakyan/auth/internal/model"
+	"github.com/levon-dalakyan/auth/internal/service"
+	serviceMocks "github.com/levon-dalakyan/auth/internal/service/mocks"
+	desc "github.com/levon-dalakyan/auth/pkg/user_v1"
+)
+
+func TestGet(t *testing.T) {
+	t.Parallel()
+	type usersServiceMockFunc func(mc *minimock.Controller) service.UsersService
+
+	type args struct {
+		ctx context.Context
+		req *desc.GetRequest
+	}
+
+	var (
+		ctx = context.Background()
+		mc  = minimock.NewController(t)
+
+		id        = gofakeit.Int64()
+		name      = gofakeit.Name()
+		email     = gofakeit.Email()
+		role      = desc.Role(gofakeit.RandomInt([]int{0, 1}))
+		createdAt = gofakeit.Date()
+		updatedAt = gofakeit.Date()
+
+		serviceErr = fmt.Errorf("service error")
+
+		req = &desc.GetRequest{
+			Id: id,
+		}
+
+		serviceRes = &model.UserFullNoPass{
+			Id:        id,
+			Name:      name,
+			Email:     email,
+			Role:      role,
+			CreatedAt: createdAt,
+			UpdatedAt: sql.NullTime{
+				Time:  updatedAt,
+				Valid: true,
+			},
+		}
+
+		res = &desc.GetResponse{
+			Id:        id,
+			Name:      name,
+			Email:     email,
+			Role:      role,
+			CreatedAt: timestamppb.New(createdAt),
+			UpdatedAt: timestamppb.New(updatedAt),
+		}
+	)
+
+	tests := []struct {
+		name             string
+		args             args
+		want             *desc.GetResponse
+		err              error
+		usersServiceMock usersServiceMockFunc
+	}{
+		{
+			name: "success case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: res,
+			err:  nil,
+			usersServiceMock: func(mc *minimock.Controller) service.UsersService {
+				mock := serviceMocks.NewUsersServiceMock(mc)
+				mock.GetMock.Expect(ctx, id).Return(serviceRes, nil)
+				return mock
+			},
+		},
+		{
+			name: "service error case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: nil,
+			err:  serviceErr,
+			usersServiceMock: func(mc *minimock.Controller) service.UsersService {
+				mock := serviceMocks.NewUsersServiceMock(mc)
+				mock.GetMock.Expect(ctx, id).Return(nil, serviceErr)
+				return mock
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			usersServiceMock := tt.usersServiceMock(mc)
+			api := users.NewImplementation(usersServiceMock)
+
+			user, err := api.Get(tt.args.ctx, tt.args.req)
+			require.Equal(t, tt.err, err)
+			require.Equal(t, tt.want, user)
+		})
+	}
+}
