8e76bc25f5e88fad4048d6d230ce649c92ad9efa
levon-dalakyan <wman8800@gmail.com>
2025-07-21 13:10:46 +0300

    improvement

--- internal/api/users/create.go
+++ internal/api/users/create.go
@@ -10,8 +10,11 @@ import (
 
 func (i *Implementation) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
 	userId, err := i.usersService.Create(ctx, converter.ToUserFromDesc(req))
+	if err != nil {
+		return nil, err
+	}
 
 	return &desc.CreateResponse{
 		Id: userId,
-	}, err
+	}, nil
 }
